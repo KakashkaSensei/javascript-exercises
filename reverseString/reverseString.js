ab14a2e1c55723b59819aee962a9aee5949ca8db
Kakashka <Ron.liberman.96@gmail.com>
2021-07-17 19:03:05 +0300

    reverseString solved

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str)
+{
+    var arr = str.split('');
+    var nstr = "";
+    var x = str.length-1;
+    while(x>=0)
+    {
+        nstr+=arr[x]
+        x--;
+    }
+    return nstr;
 };
 
 module.exports = reverseString;
