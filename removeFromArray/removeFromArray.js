281c7de264aab53378ae03e9bab7c01b85435b2b
Kakashka <Ron.liberman.96@gmail.com>
2021-07-19 22:17:02 +0300

    removeFromArray solved

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function() 
+{
+    var i = 0;
+    var b = 1;
+    var arr = arguments[0];
+    while(b<=arguments.length)    
+    {
+        i = 0;
+        while(i<arr.length)
+        {
+        if(arr[i]==arguments[b] && typeof arr[i] == typeof arguments[b])
+        {
+            arr.splice(i,1);
+            i--;
+        }
+        i++;
+        }
+        b++;
+    }
+    return arr;
 };
 
 module.exports = removeFromArray;
