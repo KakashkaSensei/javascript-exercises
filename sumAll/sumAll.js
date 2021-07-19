5241c89624a2314902c6972a8a04d91583c64ddd
Kakashka <Ron.liberman.96@gmail.com>
2021-07-19 22:58:12 +0300

    sumAll solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,43 @@
-const sumAll = function() {
 
-};
+var sumAll = function()
+{    
+    if(arguments[0]>0 && arguments[1]>0 && typeof arguments[0]=="number" && typeof arguments[1]=="number")   
+    {
+        if (arguments[0]<arguments[1])
+        {
+            var num = arguments[0];
+            var num2 = arguments[1];
+            var i = num;
+            var x = num;
+            var sum = 0;
+            while(i <= num2)
+            {
+                sum += x;
+                i++;
+                x++; 
+            }
+        }
+        else
+        {
+            var num = arguments[1];
+            var num2 = arguments[0];
+            var i = num;
+            var x = num;
+            var sum = 0;
+            while(i <= num2)
+            {
+                sum += x;
+                i++;
+                x++; 
+            } 
+        }
+    }
+    else
+    {
+        return "ERROR";
+    }
+    return sum;
+}
+;
 
 module.exports = sumAll;
